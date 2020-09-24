c53afaf0e28a371649b4004f4dd50625c4f71993
Aras <amvitkus@gmail.com>
2020-09-24 14:35:02 -0400

    Added chapter 8 project data.

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,8 +1,43 @@
+import kotlin.math.roundToInt
 const val TAVERN_NAME = "Bomboe's Place"
 
+var playerGold = 10
+var playerSilver = 90
+var dragonBreathGallons = 5.0
+const val PINT = 0.125
+var dragonBreathPints = dragonBreathGallons / PINT
+var dragonBreathOrders = 0
+
 fun main() {
-    placeOrder("shandy,Dragon's Breath,5.91");
-    //placeOrder("elixer,Shirley's Temple,4.20")
+    for (i in 1..10) {
+        println("##### Order Number: $i #####")
+        placeOrder("shandy,Dragon's Breath,5.91");
+        //placeOrder("elixer,Shirley's Temple,4.20")
+    }
+}
+
+fun performPurchase(price: Double){
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("Total purse: $totalPurse")
+    println("Purchasing item for $price")
+
+    val remainingBalance = totalPurse - price
+    if (remainingBalance > 0) {
+        println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+
+        val remainingGold = remainingBalance.toInt()
+        val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+        playerGold = remainingGold
+        playerSilver = remainingSilver
+        displayBalance()
+    } else {
+        println("Not enough gold to make a purchase.")
+    }
+}
+
+private fun displayBalance() {
+    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -22,6 +57,10 @@ private fun toDragonSpeak(phrase: String) =
             }
         }
 
+private fun displayDragonBreathPintsLeft() {
+    println("Dragon's Breath pints left: ${dragonBreathPints.toInt()}")
+}
+
 private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
@@ -31,10 +70,18 @@ private fun placeOrder(menuData: String) {
     val message = "Boromir buys a $name ($type) for $price."
     println(message)
 
+    performPurchase(price.toDouble())
+
     val phrase = if (name == "Dragon's Breath") {
         "Boromir exclaims ${toDragonSpeak("Ah, fantastic $name!")}"
     } else {
         "Boromir says: Thanks for the $name."
     }
     println(phrase)
+
+    if (name == "Dragon's Breath") {
+        dragonBreathOrders++
+        dragonBreathPints--
+        if (dragonBreathOrders >= 8) displayDragonBreathPintsLeft()
+    }
 }
